1e4effac72bdb0ceb9a492788b0bb8cfb1a998b5
sakibhossen202 <67454665+sakibhossen202@users.noreply.github.com>
2020-08-09 02:51:07 +0600

    bind method

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+//object method property review
 const normalPerson ={
     firstName: "Sakib",
     lastName:"Hossen",
@@ -17,3 +18,27 @@ normalPerson.billCharge(12000);
 normalPerson.billCharge(50000)
 normalPerson.billCharge(10000)
 console.log(normalPerson.salary);
+
+//bind method in object dhar korjo
+
+const loverPerson ={
+    firstName: "Niamul",
+    lastName:"Hossen",
+    salary :10000
+
+}
+const loverPersonSalary = normalPerson.billCharge.bind(loverPerson)
+loverPersonSalary(1200)
+loverPersonSalary(2000)
+console.log(loverPerson.salary)
+
+const friendlyPerson ={
+    firstName: "Habib",
+    lastName:"Hossen",
+    salary :18000
+}
+
+const friendlyPersonCharge = normalPerson.billCharge.bind(friendlyPerson)
+friendlyPersonCharge(1000)
+console.log(friendlyPersonCharge(2000))
+console.log(friendlyPerson.salary)
